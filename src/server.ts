dbbbf10e99b8bf90d17ec7e6d16e0e17175ca817
Aylon <aylonc1@gmail.com>
2025-05-30 22:34:48 +0300

    starting to add logs

--- src/server.ts
+++ src/server.ts
@@ -5,26 +5,39 @@ import { handleCommand } from './commands';
 const PORT = 6379;
 
 const server = net.createServer((socket) => {
-  console.log('Client connected');
+   const clientAddress = `${socket.remoteAddress}:${socket.remotePort}`;
+   console.log(`[+] Client connected: ${clientAddress}`);
 
   socket.on('data', (data) => {
     const args= parseRESP(data);
 
+     console.log(`[>] Received from ${clientAddress}:`, data.toString().replace(/\r\n/g, '\\r\\n'));
+
     if(!args || args.length === 0){
-        socket.write('-ERR invalid conmad\r\n');
+         const errorMsg = "-ERR invalid input\r\n";
+         console.log(`[<] Sent to ${clientAddress}: ${errorMsg.trim()}`);
+        socket.write(errorMsg);
         return;
     }
    
-    const respone = handleCommand(args);
+    const response = handleCommand(args);
 
-    socket.write(respone);
+
+    console.log(`[CMD]`, args);
+    console.log(`[<] Sent to ${clientAddress}: ${response.trim()}`);
+    socket.write(response);
     
 
   });
 
   socket.on('end', () => {
-    console.log('Client disconnected');
+      console.log(`[-] Client disconnected: ${clientAddress}`);
   });
+
+ socket.on('error', (err) => {
+    console.error(`[!] Error with ${clientAddress}:`, err.message);
+  });
+
 });
 
 server.listen(PORT, () => {
